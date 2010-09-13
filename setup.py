c6b1fc953dabae81ac8dcabce1f5f756d2aabf1e
Mike Korobov <kmike84@gmail.com>
2010-09-14 02:27:18 +0600

    Initial import

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+version='1.0'
+
+setup(
+    name='django-excel-response',
+    version=version,
+    author='Tarken',
+    maintainer = 'Mikhail Korobov',
+    maintainer_email='kmike84@gmail.com',
+
+    packages=['excel_response'],
+
+    url='http://bitbucket.org/kmike/django-excel-response/',
+    download_url = 'http://bitbucket.org/kmike/django-excel-response/get/tip.zip',
+    description = """A subclass of HttpResponse which will transform a QuerySet,
+or sequence of sequences, into either an Excel spreadsheet or
+CSV file formatted for Excel, depending on the amount of data.
+
+http://djangosnippets.org/snippets/1151/
+""",
+
+    long_description = open('README.rst').read(),
+
+    required = ['xlwt'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+
+    ],
+)
