2d424a4d5e14f3f6d3ec7e5ab8d3db4b7f20968c
Joey Wilhelm <jwilhelm@godaddy.com>
2017-04-27 17:42:22 -0700

    Do lots of testing!

--- testapp/tests.py
+++ testapp/tests.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import, unicode_literals
+
+from collections import OrderedDict
+
 from django.test import TestCase
 
 from excel_response import response
@@ -5,10 +9,12 @@ from excel_response import response
 from .models import TestModel
 
 
-class ExcelResponseTest(TestCase):
+class ExcelResponseCSVTest(TestCase):
 
     def test_force_csv(self):
         r = response.ExcelResponse([['a']], force_csv=True)
+        # Call this so that all of the data gets resolved
+        r.streaming_content
         self.assertEqual(r['content-type'], 'text/csv; charset=utf8')
 
     def test_exceeding_row_limit_with_list_creates_csv(self):
@@ -17,6 +23,8 @@ class ExcelResponseTest(TestCase):
         r = response.ExcelResponse(
             [['a'], ['b'], ['c']]
         )
+        # Call this so that all of the data gets resolved
+        r.streaming_content
         self.assertEqual(r['content-type'], 'text/csv; charset=utf8')
         response.ROW_LIMIT = old_limit
 
@@ -26,6 +34,8 @@ class ExcelResponseTest(TestCase):
         r = response.ExcelResponse(
             [['a', 'b', 'c']]
         )
+        # Call this so that all of the data gets resolved
+        r.streaming_content
         self.assertEqual(r['content-type'], 'text/csv; charset=utf8')
         response.COL_LIMIT = old_limit
 
@@ -38,18 +48,59 @@ class ExcelResponseTest(TestCase):
         r = response.ExcelResponse(
             TestModel.objects.all()
         )
+        # Call this so that all of the data gets resolved
+        r.streaming_content
         self.assertEqual(r['content-type'], 'text/csv; charset=utf8')
         response.ROW_LIMIT = old_limit
 
-    def test_exceeding_column_limit_with_list_creates_csv(self):
+    def test_exceeding_column_limit_with_model_creates_csv(self):
         old_limit = response.COL_LIMIT
         response.COL_LIMIT = 2
         TestModel.objects.create(text='a', number='1')
         r = response.ExcelResponse(
             TestModel.objects.all()
         )
+        # Call this so that all of the data gets resolved
+        r.streaming_content
         self.assertEqual(r['content-type'], 'text/csv; charset=utf8')
         response.COL_LIMIT = old_limit
+
+    def test_csv_from_list(self):
+        r = response.ExcelResponse(
+            [['a', 'b', 'c'], [1, 2, 3], [4, 5, 6]],
+            force_csv=True
+        )
+        output = r.getvalue()
+        self.assertEqual(
+            output,
+            b'a,b,c\r\n1,2,3\r\n4,5,6\r\n'
+        )
+
+    def test_csv_from_list_of_dicts(self):
+        r = response.ExcelResponse(
+            [
+                OrderedDict([('a', 1), ('b', 2), ('c', 3)]),  # OrderedDict ensures the order of our headers & output
+                {'a': 4, 'b': 5, 'c': 6}
+            ],
+            force_csv=True
+        )
+        output = r.getvalue()
+        self.assertEqual(
+            output,
+            b'a,b,c\r\n1,2,3\r\n4,5,6\r\n'
+        )
+
+
+class ExcelResponseExcelTest(TestCase):
+
+    def test_create_excel_from_list(self):
+        pass
+
+    def test_create_excel_from_list_of_dicts(self):
+        pass
+
+    def test_create_excel_from_queryset(self):
+        pass
 
     def test_header_font_is_applied(self):
         pass
